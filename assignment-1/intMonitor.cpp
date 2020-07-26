c0848eaf5cad87070a5630d9b28755c6374a216c
HavanaKaba <hkaba1@myseneca.ca>
2020-07-26 11:47:39 -0700

    File Setup

---
+++ assignment-1/intMonitor.cpp
@@ -0,0 +1,22 @@
+// intMonitor.cpp
+// By: Havana Kaba & Bruno Alexander
+// For Miguel Watler
+// Class: DPS912
+// Assignment #1
+// July 31, 2020
+
+#include <fcntl.h>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    return 0;
+
+}
