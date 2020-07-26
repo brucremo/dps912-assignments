4511fda02376f2da629e9a4c26d5bc53c76b1447
HavanaKaba <hkaba1@myseneca.ca>
2020-07-26 13:00:14 -0700

    Basic Setup

--- assignment-1/intMonitor.cpp
+++ assignment-1/intMonitor.cpp
@@ -9,14 +9,44 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    return 0;
+char pathname[13] = "/tmp/assign1";
 
+void error(const char *message);
+
+int main(int argc, char *argv[]) {
+    int sock, slen, command;
+    struct sockaddr_un s_addr;
+
+    // Setup the Socket //
+    bzero((char *)&s_addr, sizeof(s_addr));
+    s_addr.sun_family = AF_UNIX;
+    strcpy(s_addr.sun_path, pathname);
+    slen = strlen(s_addr.sun_path) + sizeof(s_addr.sun_family);
+    if ((sock = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
+        error("Could not create socket");
+    }
+
+    // Connect to the Network Monitor //
+    if (connect(sock, (struct sockaddr *) &s_addr, slen) < 0) {
+        error("Could not connect to server");
+    }
+
+    // Ready to Start //
+    
+
+
+    return 0;
+}
+
+void error(const char *message) {
+    perror(message);
+    exit(0);
 }
--- assignment-1/netMonitor.cpp
+++ assignment-1/netMonitor.cpp
@@ -9,13 +9,67 @@
 #include <fstream>
 #include <cstring>
 #include <iostream>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+char pathname[13] = "/tmp/assign1";
+
+static void sigHandler(int sig);
+void error(const char *message);
+
+int main(int argc, char *argv[]) {
+    int masterfd, recv, slen;
+    socklen_t clen;
+    struct sockaddr_un s_addr, c_addr;
+    char buffer[128];
+
+    // Set Socket File Descriptor //
+    if ((masterfd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
+        error("Could not create socket");
+    }
+
+    // Setup the Socket //
+    bzero((char *) &s_addr, sizeof(s_addr));
+    s_addr.sun_family = AF_UNIX;
+    strcpy(s_addr.sun_path, pathname);
+    slen = strlen(s_addr.sun_path) + sizeof(s_addr.sun_family);
+
+    // Bind the Socket //
+    if ((bind(masterfd, (struct sockaddr *)&s_addr, slen)) < 0) { 
+        error("Error binding the socket");
+    }
+
+    // Listen on Socket //
+    listen(masterfd, 5);
+    cout << "Listening on socket..." << endl;
+
+    // Accept the Connection //
+    clen = sizeof(c_addr);
+    recv = accept(masterfd, (struct sockaddr *)&c_addr, &clen);
+    if (recv < 0) { error("Could not accept connection."); }
+    
     return 0;
+}
+
+static void sigHandler(int sig) {
+    switch(sig) {
+        case SIGINT:
+            cout << "CTRL-C Interrution... Shutting Down..." << endl;
+            break;
+        case SIGTSTP:
+            cout << "intfMonitor: ctrl-Z discarded" << endl;
+            break;
+	default:
+	    cout << "Undefined signal..." << endl;
+    }
+}
+
+void error(const char *message) {
+    perror(message);
+    exit(0);
 }
